7291f9fedc3de09c4472289901cb8a2fc4c05481
Cheez <cooltek6@pm.me>
2025-08-24 15:08:59 -0600

    maintest. add a textbox for work to be done description

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+#import unittest
+
+class TestMain(unittest.TestCase):
+    pass
+    # def test_print_form(self):
+    #     from main import ServiceOrder
+    #     custy_info = {
+    #         'name': 'John Doe',
+    #         'address': '123 Main St',
+    #         'city': 'Anytown',
+    #         'zipcode': '12345',
+    #         'phone': '555-1234',
+    #         'email': '
